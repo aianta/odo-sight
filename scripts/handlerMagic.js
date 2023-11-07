27c399e91d21e3a38f60d78df712ca44d0ebd214
Alex Ianta <aianta@dal.ca>
2023-11-07 08:21:47 -0700

    cleanup

--- scripts/handlerMagic.js
+++ scripts/handlerMagic.js
@@ -3,25 +3,20 @@
 /**
  * Event Listener Hijack for accurate cause-effect logging. 
  * Key concept: Need to make sure logging event handlers fire BEFORE any dom changes take place to get an accurate picture of the DOM at event time.
+ * 
+ * See relevant stack overflow threads:
+ * https://stackoverflow.com/questions/446892/how-to-find-event-listeners-on-a-dom-node-in-javascript-or-in-debugging/6434924#6434924
+ * https://stackoverflow.com/questions/77434759/hijacking-addeventlistener-results-in-too-much-recursion-when-adding-a-react-inv/77439399#77439399
  */
 
-// function reportIn(e){
-//     var a = this.lastListenerInfo[this.lastListenerInfo.length-1]
-//     console.log(`reportIn this = ${this.toString()} a = ${a.toString()}`)
-//     console.log('reportIn')
-//     // console.log(a)
-//   }
 
-console.log(Node.prototype.addEventListener.toString())
 
 const handlerMagic = new Promise((resolve)=>{
     Node.prototype.realAddEventListener = Node.prototype.addEventListener;
     resolve()
 }).then(_=>{
     Node.prototype.addEventListener = function(a,b,c){
-        //console.log(this)
-        //console.log(`Node.prototype.addEventListener this = ${this.toString()} a = ${a.toString()} b = ${b.toString()}`)
-    //    this.realAddEventListener(a, reportIn, c)
+
         this.realAddEventListener(a,b,c)
         
         if(!this.lastListenerInfo){
@@ -29,9 +24,8 @@ const handlerMagic = new Promise((resolve)=>{
         };
         this.lastListenerInfo.push({a:a, b:b, c:c})
     
-        // this.lastListenerInfo.forEach(listener=>console.log(""+listener.b.toString()))
         let logUIIndex = this.lastListenerInfo.findIndex(listener=>listener.b.toString().startsWith('function (browserEvent) {'))
-        // console.log('logUIIndex: ' + logUIIndex)
+
         let logUIListener = this.lastListenerInfo[logUIIndex]
     
         if(logUIIndex !== -1 || logUIIndex === 0){
