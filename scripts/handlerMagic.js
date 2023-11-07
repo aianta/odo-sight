891d7dff30e6a3b02d24f41778f8e6cc56634b1e
Alex Ianta <aianta@dal.ca>
2023-11-07 08:19:58 -0700

    fixed pesky race condition in handlerMagic.js

--- scripts/handlerMagic.js
+++ scripts/handlerMagic.js
@@ -2,7 +2,7 @@
 
 /**
  * Event Listener Hijack for accurate cause-effect logging. 
- * Key concept: Need to make sure logging event handlers fire BEFORE any dom changes take place.
+ * Key concept: Need to make sure logging event handlers fire BEFORE any dom changes take place to get an accurate picture of the DOM at event time.
  */
 
 // function reportIn(e){
@@ -11,41 +11,50 @@
 //     console.log('reportIn')
 //     // console.log(a)
 //   }
-  
-Node.prototype.realAddEventListener = Node.prototype.addEventListener;
 
-Node.prototype.addEventListener = function(a,b,c){
-//    console.log(`Node.prototype.addEventListener this = ${this.toString()} a = ${a.toString()} b = ${b.toString()}`)
-//    this.realAddEventListener(a, reportIn, c)
-    this.realAddEventListener(a,b,c)
-    
-    if(!this.lastListenerInfo){
-        this.lastListenerInfo = new Array()
-    };
-    this.lastListenerInfo.push({a:a, b:b, c:c})
+console.log(Node.prototype.addEventListener.toString())
 
-    // this.lastListenerInfo.forEach(listener=>console.log(""+listener.b.toString()))
-    let logUIIndex = this.lastListenerInfo.findIndex(listener=>listener.b.toString().startsWith('function (browserEvent) {'))
-    // console.log('logUIIndex: ' + logUIIndex)
-    let logUIListener = this.lastListenerInfo[logUIIndex]
-
-    if(logUIIndex !== -1 || logUIIndex === 0){
-        let newLastListenerInfo = []
-        //If we do have a LogUI handler here that's not already in first place. Let's ensure it executes first by:
-        //removing all other listeners, adding the logUI listener, then adding the other listeners back.
-        let otherListeners = this.lastListenerInfo.filter(listener=>!listener.b.toString().startsWith('function (browserEvent) {'))
-        for (let listener of otherListeners){
-          this.removeEventListener(listener.a, listener.b, listener.c)
-        }
-        this.realAddEventListener(logUIListener.a, logUIListener.b, logUIListener.c)
-        newLastListenerInfo.push({a:logUIListener.a, b:logUIListener.b, c:logUIListener.c})
+const handlerMagic = new Promise((resolve)=>{
+    Node.prototype.realAddEventListener = Node.prototype.addEventListener;
+    resolve()
+}).then(_=>{
+    Node.prototype.addEventListener = function(a,b,c){
+        //console.log(this)
+        //console.log(`Node.prototype.addEventListener this = ${this.toString()} a = ${a.toString()} b = ${b.toString()}`)
+    //    this.realAddEventListener(a, reportIn, c)
+        this.realAddEventListener(a,b,c)
         
-        for (let listener of otherListeners){
-          this.realAddEventListener(listener.a, listener.b, listener.c)
-          newLastListenerInfo.push({a:listener.a, b:listener.b, c:listener.c})
+        if(!this.lastListenerInfo){
+            this.lastListenerInfo = new Array()
+        };
+        this.lastListenerInfo.push({a:a, b:b, c:c})
+    
+        // this.lastListenerInfo.forEach(listener=>console.log(""+listener.b.toString()))
+        let logUIIndex = this.lastListenerInfo.findIndex(listener=>listener.b.toString().startsWith('function (browserEvent) {'))
+        // console.log('logUIIndex: ' + logUIIndex)
+        let logUIListener = this.lastListenerInfo[logUIIndex]
+    
+        if(logUIIndex !== -1 || logUIIndex === 0){
+            let newLastListenerInfo = []
+            //If we do have a LogUI handler here that's not already in first place. Let's ensure it executes first by:
+            //removing all other listeners, adding the logUI listener, then adding the other listeners back.
+            let otherListeners = this.lastListenerInfo.filter(listener=>!listener.b.toString().startsWith('function (browserEvent) {'))
+            for (let listener of otherListeners){
+              this.removeEventListener(listener.a, listener.b, listener.c)
+            }
+            this.realAddEventListener(logUIListener.a, logUIListener.b, logUIListener.c)
+            newLastListenerInfo.push({a:logUIListener.a, b:logUIListener.b, c:logUIListener.c})
+            
+            for (let listener of otherListeners){
+              this.realAddEventListener(listener.a, listener.b, listener.c)
+              newLastListenerInfo.push({a:listener.a, b:listener.b, c:listener.c})
+            }
+            this.lastListenerInfo = newLastListenerInfo
         }
-        this.lastListenerInfo = newLastListenerInfo
+    
+    
     }
+    console.log('handlerMagic.js ran!')
+})
 
 
-}
